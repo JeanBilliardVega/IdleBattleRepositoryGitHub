a68bbea69e56670b2f939bf6f8b66afe264a7bd8
JeanBilliardVega <163461785+JeanBilliardVega@users.noreply.github.com>
2024-06-06 21:43:18 +0000

    Menu 0.1

--- main.c
+++ main.c
@@ -2,12 +2,35 @@
 
 void mostrarMenu()
 {
-  printf("Ingresa tu opción:\n");
-  printf("1. Pelear");
+  puts("Ingresa tu opción:\n");
+  puts("1. Pelear");
+  puts("2. Summon");
+  puts("3. Personajes");
+  puts("4. Borrar Cuenta Definitivamente (No se puede deshacer)");
 }
 
 
 int main(void) {
-  mostrarMenu();
+  char opcion;
+  do
+  {
+    mostrarMenu();
+    scanf("%c", &opcion);
+    switch(opcion)
+    {
+      case '1':
+        printf("peleando");
+        break;
+      case '2':
+        printf("summoneando");
+        break;
+      case '3':
+        printf("personajes");
+        break;
+      case '4':
+        printf("eliminando cuenta");
+        break;
+    }
+  }while(opcion != 4);
   
 }
